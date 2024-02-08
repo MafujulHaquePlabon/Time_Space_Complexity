ed0e49179587c7f21fa59588d192e95070b40a2d
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2024-02-08 22:49:24 +0600

    time complexity

---
+++ timeC.js
@@ -0,0 +1,6 @@
+Time Complexity
+1. Assignment operation [a=b, x=10][O(1)]
+2. comparison operation [a>b][O(1)]
+3. Mathematical operation [2+6][O(1)]
+4. function call [r=f(n)]
+5. work inside function [Multiply]
